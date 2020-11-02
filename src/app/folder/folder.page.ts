7096abd97237fac14e78eb89ebc1afbd9924ab2a
Nickr <nickrgamer04@gmail.com>
2020-11-03 05:09:44 +0800

    policy servicing

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -7,12 +7,82 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./folder.page.scss'],
 })
 export class FolderPage implements OnInit {
-  public folder: string;
+  navs: any = [
+    {
+      'id': 1,
+      'icon': 'icon_fund.png',
+      'label': 'Manage ILAS Funds'
+    },
+    {
+      'id': 2,
+      'icon': 'icon_claims.png',
+      'label': 'Claims'
+    },
+    {
+      'id': 3,
+      'icon': 'icon_serving.png',
+      'label': 'Policy Servicing'
+    }
+  ];
+
+  services: any = [
+    {
+      'icon': 'documents-outline',
+      'label': 'Move value from one policy to another',
+      'actions': [
+        {
+          'desc': 'Pay with Policy Value'
+        },
+        {
+          'desc': 'Standing Instructions'
+        }
+      ]
+    },
+    {
+      'icon': 'wallet-outline',
+      'label': 'Cash out your policy values',
+      'actions': [
+        {
+          'desc': 'Value Withdrawal'
+        },
+        {
+          'desc': 'Payout Method'
+        }
+      ]
+    },
+    {
+      'icon': 'cash-outline',
+      'label': 'Take a loan against your policy value',
+      'actions': [
+        {
+          'desc': 'Loans'
+        }
+      ]
+    },
+    {
+      'icon': 'pricetags-outline',
+      'label': 'Payment options for your premiums',
+      'actions': [
+        {
+          'desc': 'Premium Payment Method'
+        }
+      ]
+    }
+  ]
+
+  activeView = 0;
 
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+  navigate(navId: any) {
+    this.activeView = navId;
+  }
+
+  back() {
+    this.activeView = 0;
   }
 
 }
