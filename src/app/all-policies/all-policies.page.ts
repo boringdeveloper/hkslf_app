cafa1bdafe1d05f4686b789490d60ea0f790eafd
Nickr <nickrgamer04@gmail.com>
2020-11-02 09:16:17 +0800

    initialize policies page

---
+++ src/app/all-policies/all-policies.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-policies',
+  templateUrl: './all-policies.page.html',
+  styleUrls: ['./all-policies.page.scss'],
+})
+export class AllPoliciesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
